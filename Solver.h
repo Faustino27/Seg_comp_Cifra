7401364b32307a438147f8dd290eafbd30da944e
Gabriel Faustino <60712546+Faustino27@users.noreply.github.com>
2022-12-11 00:08:23 -0300

    começo

--- Solver.h
+++ Solver.h
@@ -1,7 +1,6 @@
 #include <vector>
 
-class Solver
-{
+class Solver{
 private:
     std::vector<double> en = {0.08167,0.01492,0.02782,0.04253,0.12702, 0.02228, 0.02015,
     0.06094, 0.06966, 0.00153,0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095,
@@ -11,8 +10,7 @@ private:
     0.06530, 0.06805, 0.04336, 00.3639, 0.01575, 0.00037, 0.00253, 0.00006, 0.00470};
 
 
-    std::pair<std::vector<int>, int> numeroOcorrencias(std::string cifrado)
-    {
+    std::pair<std::vector<int>, int> numeroOcorrencias(std::string cifrado){
         std::vector<int> resultado;
         int tamanho = cifrado.size();
         int total = 0;
@@ -34,18 +32,48 @@ private:
         return {resultado, total/tamanho};
     }
 
-    std::vector<int> frequenciaSpike(std::vector<int> ocorrencias, int media)
-    {
+    std::vector<int> frequenciaSpike(std::vector<int> ocorrencias, int media){
         int tamanho = ocorrencias.size();
         std::vector<int> frequencia;
         //std::cout << "media:  " << media << std::endl;
 
-        for (int i = 0; i < tamanho; i++)
-        {
+        for (int i = 0; i < tamanho; i++){
             std::cout << ocorrencias[i] << " ";
             if(ocorrencias[i] > media) frequencia.push_back(i);
         }
         return frequencia;
+    }
+
+    std::string compute(int tChave, std::string cifrado, int lingua){
+        std::string res = "";
+        for(int t = 0;t < tChave;t++){
+            std::vector<int> letras(26,0);
+            int total=0;
+            for(int i = t;i < cifrado.size();i += tChave){
+                letras[cifrado[i]]++;
+                total++;
+            }
+            std::vector<double> frequenciaCifrado;
+            for(int i = 0;i < letras.size();i++){
+                frequenciaCifrado.push_back((letras[i]/(double)total));
+            }
+            std::vector<double> frequenciaAlfabeto = pt; 
+            if(lingua)frequenciaAlfabeto = en;
+            double maior = 0.0;
+            int shift = 0, tFreq= frequenciaAlfabeto.size();
+            for(int i = 0;i < tFreq;i++){
+                double multi = 0.0;
+                for(int j = 0;j<tFreq;j++){
+                    multi += frequenciaAlfabeto[(j+i)%tFreq]*frequenciaCifrado[j];
+                }
+                if(multi > maior){
+                    maior = multi;
+                    shift = i;
+                }
+            }
+            res += 'a'+shift;
+        }
+        return res;
     }
 
 public:
@@ -59,20 +87,16 @@ public:
         // o resultado esprado
         tie(ocorrencias, media) = numeroOcorrencias(cifrado);
         std::vector<int> frequencia =   frequenciaSpike(ocorrencias, media);
-        std::cout << std::endl;
-        //for(auto x: frequencia) std::cout << x << " ";
 
         return "";
     }
 
     // construtores
-    Solver()
-    {
+    Solver(){
         Tabela cifra();
     }
 
-    Solver(std::string alfabeto)
-    {
+    Solver(std::string alfabeto){
         Tabela cifra(alfabeto);
         // não sei se vai precisar das funções dessa classe, mas deixei ai caso precise
     }
